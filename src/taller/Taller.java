8dfc8db9e266e7a08d61c0451f43a5b993228e4c
Juan <jj.monte2002@gmail.com>
2024-10-28 20:33:17 -0500

    First commit

--- src/taller/Taller.java
+++ src/taller/Taller.java
@@ -7,8 +7,8 @@ public class Taller {
     
     public static void main(String[] args) {
         
-        inicio o1=new inicio();
-        o1.setVisible(true);
+        inicio objeto=new inicio();
+        objeto.setVisible(true);
         
     }
     
